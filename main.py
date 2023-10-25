9d22ee3133227303bf488e30cc29520e66bb279b
JavaCoffeeTime <camille.gaschet1984@gmail.com>
2023-10-25 09:19:45 +0200

    positioning whole ships on the grid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from colorama import Fore, Style
 
 
@@ -15,45 +14,45 @@ def init_board():
 
 
 def print_board(board):
-    print("\n\n  1 2 3 4 5 6 7 8 9 10")  # Added two newlines before the board
+    print("\n\n  A B C D E F G H I J")
     for i in range(10):
-        print(chr(65 + i), end=" ")
+        print(i+1, end=" ")
         for j in range(10):
-            if board[i][j] == 'X':
-                print(Fore.RED + board[i][j] + Style.RESET_ALL, end=" ")
-            elif board[i][j] == 'O':
-                print(Fore.BLUE + board[i][j] + Style.RESET_ALL, end=" ")
+            if board[j][i] == 'X':
+                print(Fore.RED + board[j][i] + Style.RESET_ALL, end=" ")
+            elif board[j][i] == 'O':
+                print(Fore.BLUE + board[j][i] + Style.RESET_ALL, end=" ")
             else:
-                print(board[i][j], end=" ")
+                print(board[j][i], end=" ")
         print()
 
 
-def place_ship(board, row, col, ship):
-    if 0 <= row < 10 and 0 <= col < 10:
-        board[row][col] = ship
+def place_ship(board, start_row, start_col, end_row, end_col, ship):
+    for i in range(start_row, end_row + 1):
+        for j in range(start_col, end_col + 1):
+            board[j][i] = ship
 
 
 def main():
     board = init_board()
-    # Place the ships
-    place_ship(board, 1, 1, 'A')  # Aircraft carrier
-    place_ship(board, 0, 3, 'C')  # Cruiser
-    place_ship(board, 2, 4, 'D')  # Destroyer
-    place_ship(board, 7, 4, 'S')  # Submarine
-    place_ship(board, 4, 8, 'T')  # Torpedo boat
+    place_ship(board, 1, 1, 1, 5, 'A')  # Aircraft carrier
+    place_ship(board, 3, 0, 6, 0, 'C')  # Cruiser
+    place_ship(board, 4, 2, 6, 2, 'D')  # Destroyer
+    place_ship(board, 4, 7, 4, 9, 'S')  # Submarine
+    place_ship(board, 8, 4, 8, 5, 'T')  # Torpedo boat
 
     while True:
         print_board(board)
         print("Enter your shot position:")
         shot = input().upper()
-        row = ord(shot[0]) - 65
-        col = int(shot[1:]) - 1
-        if board[row][col] != ' ':
+        col = ord(shot[0]) - 65
+        row = int(shot[1:]) - 1
+        if board[col][row] != ' ':
             print("You've touched the ship!")
-            board[row][col] = 'X'
+            board[col][row] = 'X'
         else:
             print("You've missed the ship!")
-            board[row][col] = 'O'
+            board[col][row] = 'O'
 
 
 if __name__ == "__main__":
